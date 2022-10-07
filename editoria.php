06cf38c19f6f0ab0236f9c3ce7247ca789d21721
MateusMachado01 <gigicasha@gmail.com>
2022-10-07 09:41:57 -0300

    estrutura base

---
+++ editoria.php
@@ -0,0 +1,12 @@
+<?php
+include_once './includes/_header.php'
+?>
+
+    <main>
+        <h1>Editoria</h1>
+    </main>
+    
+<?php
+include_once './includes/_aside.php';
+include_once './includes/_footer.php';
+?>
