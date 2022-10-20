c51300e38b3afe9b9703371ddf17ccdc14d1041d
nath <nathalia.alencastro00@gmail.com>
2022-10-20 08:55:11 -0300

    Update _header.php

--- includes/_header.php
+++ includes/_header.php
@@ -6,13 +6,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Atualiza aí</title>
     <link rel="stylesheet" href="style.css">
+ <a href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"></a>
+ <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+ <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 </head>
 <body>
 
     <header>
-        <h1>Atualiza aí</h1>
-        <i class="fa-thin fa-comment"></i>
-        
+    <div class="container-fluid">
+    <div class="row">
+    <div class="col-11">
+    <h1>Atualiza aí</h1>
+
     <div class="container">
       <div class="btn">
         <div class="btn-left"></div>
@@ -20,6 +25,11 @@
       </div>
     </div>
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+ </div>
+    </div>
+  </div>
+        
+      
 
     </header>
 </body>
