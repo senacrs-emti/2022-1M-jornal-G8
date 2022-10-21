89107e4300609e524538d079ed232e0be3218f9b
nath <nathalia.alencastro00@gmail.com>
2022-10-21 09:23:55 -0300

    dev

--- includes/_header.php
+++ includes/_header.php
@@ -7,39 +7,14 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
   </head>
   <body class="container-fluid">
-
-    <header class="row">
-        <ol>
-            <li><a href="./index.php"></a></li>
-            <li><a href="./noticia.php"></a></li>
-            <li><a href="./editoria.php"></a></li>
-            <li><a href="./contato.php"></a></li>
-        </ol>
-    </header>
+  <header>
     <div class="row">
-    
-      <nav class="navbar navbar-light" style="background-color: #B757F2 ;">
-     <h1 class="titulo">ATUALIZA AI</h1>
-     <i class="fa-regular fa-comments"></i>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
-        </a>
-        <ul class="dropdown-menu">
-          <li><a class="dropdown-item" href="#">notícias</a></li>
-          <li><hr class="dropdown-divider"></li>
-          <li><a class="dropdown-item" href="#">artigos</a></li>
-          <li><hr class="dropdown-divider"></li>
-          <li><a class="dropdown-item" href="#">entrevistas</a></li>
-        </ul>
-      </li>
-    
-</nav>
-  <section class="top-nav">
+
     <h1 class="nana">ATUALIZA AI</h1>
     <div>
     <i class="fa-regular fa-comments"></i>
     </div>
-  </section>
+  </header>
   <div class="hamburger-menu">
     <input id="menu__toggle" type="checkbox" />
     <label class="menu__btn" for="menu__toggle">
