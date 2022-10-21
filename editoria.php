e3b9e2b4c25d74dfd982f4cb7a596af079160369
nath <nathalia.alencastro00@gmail.com>
2022-10-21 09:14:28 -0300

    main

--- editoria.php
+++ editoria.php
@@ -9,22 +9,22 @@ include_once './includes/_header.php';
       <h1>noticia1</h1>
     </div>
     <div class="col">
-      2 of 2
+      
     </div>
   </div>
   <div class="row">
     <div class="col">
-      1 of 3
+      
     </div>
     <div class="col">
-      2 of 3
+      
     </div>
     <div class="col">
-      3 of 3
+    
     </div>
   </div>
 </div>
-        <h1>Index</h1>
+      
     </main>
     
 <?php
