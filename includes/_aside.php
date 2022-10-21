67bcff1cc61a5a881bca4685996f3fae57490ef3
Michael Jackson <04446713078@senacrs.edu.br>
2022-10-21 09:23:41 -0300

    .
    .

--- includes/_aside.php
+++ includes/_aside.php
@@ -1,11 +1,2 @@
     <aside class="col-lg-4 col-12">
-        <h1 class="h1maislidos">MAIS LIDOS</h1>
-        <div class>
-        <img class="img1" src="./img/capivara-sentada-em-um-lago-durante-o-dia_181624-47544.jpg" alt="">
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non laborum nisi ea quaerat similique quos quas vitae consequatur, nam perspiciatis dolor laudantium officiis blanditiis soluta. Aut, eius minus! Eius, beatae!</p>
-        </div>
-        <div class>
-        <img class="img2" src="./depositphotos_10819690-stock-photo-portrait-of-a-young-capybara (1).jpg" alt="">
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti natus tenetur, ex aperiam, minus explicabo dignissimos quod voluptates, quisquam temporibus nemo! Reiciendis laboriosam asperiores sapiente enim animi unde optio numquam.</p>
-        </div>
     </aside>
