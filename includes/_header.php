4122199c5919f3de4befdb276bfcb30796d35deb
nath <nathalia.alencastro00@gmail.com>
2022-10-21 08:22:27 -0300

    header

--- includes/_header.php
+++ includes/_header.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="en">
+<<<<<<< Updated upstream
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -19,5 +20,32 @@
             <li><a href="./editoria.php"></a></li>
             <li><a href="contato.php"></a></li>
         </ol>
+=======
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atualiza aí</title>
+    <link rel="stylesheet" href="style.css">
+ <a href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"></a>
+ <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+ <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+</head>
+<body>
+
+    <header>
+    <div class="container-fluid">
+    <div class="row">
+    <div class="col-11">
+    <h1>Atualiza aí</h1>
+
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+ </div>
+    </div>
+  </div>
+        
+      
+
+>>>>>>> Stashed changes
     </header>
     <div class="row">
