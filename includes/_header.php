32c0611e71a628393579fd8dd604ad1e78cb739a
nath <nathalia.alencastro00@gmail.com>
2022-10-21 07:57:09 -0300

    Update _header.php

--- includes/_header.php
+++ includes/_header.php
@@ -4,14 +4,14 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
+    <a href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"></a>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 <link rel="stylesheet" href="./style.css">
    
   </head>
   <body class="container-fluid">
     <header class="row">
+      <h1 class"titulo"></h1>
         <ol>
             <li><a href="./index.php"></a></li>
             <li><a href="./noticia.php"></a></li>
