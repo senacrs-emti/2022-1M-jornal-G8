ec932b54b01af3129ffc0f1cf463ac22c8a210d3
nath <nathalia.alencastro00@gmail.com>
2022-10-21 08:25:44 -0300

    header

--- includes/_header.php
+++ includes/_header.php
@@ -1,27 +1,5 @@
 <!doctype html>
 <html lang="en">
-<<<<<<< Updated upstream
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <a href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"></a>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-<link rel="stylesheet" href="./style.css">
-   
-  </head>
-  <body class="container-fluid">
-    <header class="row">
-    <i class="fa-thin fa-comments"></i>
-      <h1 class"titulo">ATUALIZA AI</h1>
-        <ol>
-            <li><a href="./index.php"></a></li>
-            <li><a href="./noticia.php"></a></li>
-            <li><a href="./editoria.php"></a></li>
-            <li><a href="contato.php"></a></li>
-        </ol>
-=======
-<head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -45,7 +23,3 @@
   </div>
         
       
-
->>>>>>> Stashed changes
-    </header>
-    <div class="row">
