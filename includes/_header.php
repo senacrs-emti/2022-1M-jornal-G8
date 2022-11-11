9f8820daf2d209042d1269334e0cece8328638f5
nathaliaalencastro <nathalia.alencastro00@gmail.com>
2022-11-11 08:06:53 -0300

    dev

--- includes/_header.php
+++ includes/_header.php
@@ -26,7 +26,7 @@
       <div class="hamburger-menu">
     <input id="menu__toggle" type="checkbox" />
     <label class="menu__btn" for="menu__toggle">
-      <span></span>
+      
     </label>
 
     <ul class="menu__box">
