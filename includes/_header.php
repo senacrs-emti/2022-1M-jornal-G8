22209143fae94542c3bf7427421e7ebae7154063
nathaliaalencastro <nathalia.alencastro00@gmail.com>
2022-11-11 08:57:18 -0300

    dev

--- includes/_header.php
+++ includes/_header.php
@@ -9,10 +9,7 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <link rel="stylesheet" href="style.css">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="./style.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
   </head>
-  
   <body class="container-fluid">
   <header>
     <div class="row">
@@ -21,7 +18,7 @@
     <input id="menu__toggle" type="checkbox" />
     <label class="menu_btn" for="menu_toggle">
       
-    </label>
+ </label>
 
     <ul class="menu__box">
       <li><a class="menu__item" href="#">CRÔNICAS</a></li>
