7fa92c33a9aedb24ad540277bd61626bc96dfd28
nathaliaalencastro <nathalia.alencastro00@gmail.com>
2022-11-11 09:07:54 -0300

    header

--- includes/_header.php
+++ includes/_header.php
@@ -11,9 +11,7 @@
  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 </head>
 <body>
-
     <header>
-    
     <div class="row">
       <div class="col-2">
       <div class="hamburger-menu">
