65b073d0d0f1ad4d0e346d5128f4aade8d751445
nathaliaalencastro <nathalia.alencastro00@gmail.com>
2022-11-11 09:32:36 -0300

    DEV

--- includes/_header.php
+++ includes/_header.php
@@ -9,11 +9,6 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <link rel="stylesheet" href="style.css">
-    
-</head>
-<body>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <a href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"></a>
     <a href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"></a>
     <a href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"></a>
@@ -21,7 +16,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
   </head>
   <body class="container-fluid">
-  <header>
+ <header>
     <div class="row">
       <div class="col-2">
       <div class="hamburger-menu">
@@ -40,7 +35,7 @@
   </div>
       </div>
 <div class="col-8">
-    <h1 class="nana">ATUALIZA AI</h1>
+    <h1 class="nana">ATUALIZA AÍ</h1>
     </div>
     <div class="col-2">
     <i class="fa-regular fa-comments"></i>
