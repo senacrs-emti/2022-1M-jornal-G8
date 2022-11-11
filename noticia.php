208e190965b3606eaf95a7d69e23c662ae35c6ee
MateusMachado01 <108525021+MateusMachado01@users.noreply.github.com>
2022-11-11 10:00:40 -0300

    jornal

--- noticia.php
+++ noticia.php
@@ -1,12 +1,18 @@
 <?php
 include_once './includes/_header.php';
 ?>
-
-<main>
-    <h1>TITULO NOTICIA</h1>
+<link rel="stylesheet" href="style.css">
+<main id="pgNoticia">
+    <h1 class="title">capivaras assasinas matam aniversariante no seu aniversario de 16 anos</h1>
     <H3>SUBTITULO DA NOTICIA</H3>
-    <img src="includes/img/CAPIVARA.jpg" alt="">
+    <h6>nome do autor</h6>
+    <h6>data de postagem</h6>
+    <hr>
+    <img src="includes/img/CAPIVARA.jpg" alt="" class="img1">
     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta corrupti in repellendus dolorem rem ipsa vero doloremque quidem! Nihil iusto commodi sint eos molestias atque quibusdam quidem cupiditate excepturi quae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe laborum, libero minima accusamus quasi laboriosam, accusantium suscipit laudantium voluptatum explicabo maxime tempora temporibus quaerat magni quis recusandae veritatis minus quod. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus animi quo accusantium voluptatem repudiandae possimus in, dolore architecto maiores ducimus iusto atque illum similique commodi culpa porro minima! Maxime, dignissimos.</p>
+    <img src="includes/img/CAPIVARA.jpg" alt="">
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat maxime obcaecati et sapiente! Similique eligendi magnam porro, dolorem perspiciatis accusamus voluptate consequuntur nihil illo, ducimus consequatur sed recusandae esse iste? lorem</p>
     
 </main>
 
