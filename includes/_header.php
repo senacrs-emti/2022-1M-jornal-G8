d055465f8403f5a8caa564e0a8e56212495e94eb
nathaliaalencastro <nathalia.alencastro00@gmail.com>
2022-11-11 09:14:53 -0300

    header

--- includes/_header.php
+++ includes/_header.php
@@ -1,22 +1,34 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Atualiza aí</title>
-    <link rel="stylesheet" href="style.css">
- <a href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"></a>
- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
- <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="stylesheet" href="style.css">
+    
 </head>
 <body>
-    <header>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <a href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"></a>
+    <a href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"></a>
+    <a href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"></a>
+    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+  </head>
+  <body class="container-fluid">
+  <header>
     <div class="row">
       <div class="col-2">
       <div class="hamburger-menu">
     <input id="menu__toggle" type="checkbox" />
-    <label class="menu__btn" for="menu__toggle"></label>
+    <label class="menu_btn" for="menu_toggle">
+      
+    </label>
+
     <ul class="menu__box">
       <li><a class="menu__item" href="#">CRÔNICAS</a></li>
       <li><a class="menu__item" href="#">NOTÍCIAS</a></li>
@@ -26,20 +38,11 @@
     </ul>
   </div>
       </div>
-    <div class="container-fluid">
-    <div class="row">
-    <div class="col-11">
-    <h1>Atualiza aí</h1>
-    <div class="container">
-      <div class="btn">
-        <div class="btn-left"></div>
-        <div class="btn-right"></div>
-      </div>
+<div class="col-8">
+    <h1 class="nana">ATUALIZA AI</h1>
     </div>
- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
- </div>
+    <div class="col-2">
+    <i class="fa-regular fa-comments"></i>
     </div>
-  </div>
-    </header>
-</body>
-</html>
+  </header>
+  </body>
