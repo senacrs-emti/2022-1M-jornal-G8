a0d96f946e2b9641ed2458a35340882900db57ae
rossi <grossi.contatos@gmail.com>
2022-11-24 09:28:09 -0300

    rossi

--- includes/_aside.php
+++ includes/_aside.php
@@ -1,7 +1,9 @@
 <aside class="col-lg-4 col-12">   
+    <div clas="as">
     <h1 class="mais lidos"> Mais Lidos</h1>
     <img class="imagem" src="img/lulinha.jpeg">
     <p class= "aside-paragrafo">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</p>
     <img class="imagem" src="img/barbie.jfif">
     <p class="aside-paragrafo">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</p>
+</div>
 </aside>
