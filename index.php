16f26b4392c2e293eaf9349a0a33a3aa6557b325
MateusMachado01 <108525021+MateusMachado01@users.noreply.github.com>
2022-11-25 08:22:18 -0300

    recentes
    Co-Authored-By: Giovanna Pandolfo <108467195+GiovannaPandolfo@users.noreply.github.com>
    Co-Authored-By: Giovanna Rodrigues do Nascimento <108465696+gi-nascimento@users.noreply.github.com>

--- index.php
+++ index.php
@@ -7,26 +7,29 @@ include_once './includes/_header.php';
     <div class="">
         <h1>Recentes</h1>
         <hr>
-        <div class="col-lg-7"> <img class="imagem" src="img/capivarinha.jpeg" alt="">
-        <h2>Título</h2>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus suscipit eius dolor quia cum </p>
+        <div class="row">
+            <div class="col-lg-6"> 
+                <img class="imagem" src="img/capivarinha.jpeg" alt="">
+            </div>
+            <div class="col-lg-6">
+                <h2>Título</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus suscipit eius dolor quia cum </p>
+            </div>
+            
         </div>
-        <div class="col-lg-5">
-       
-        </div>
-        
-    </div>
-    <div class="">
+        <div class="">
         <hr>
-        <div class="col-lg-6"> <img class="imagem" src="img/capivarinha.jpeg" alt="">
-        </div>
-        <div class="col-lg-6">
-        <h2>Título</h2>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus suscipit eius dolor quia cum </p>
+        <div class="row">
+            <div class="col-lg-6"> 
+                <img class="imagem" src="img/capivarinha.jpeg" alt="">
+            </div>
+            <div class="col-lg-6">
+                <h2>Título</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus suscipit eius dolor quia cum </p>
+            </div>
         </div>
         <hr>
-        
-    </div>
+
 </main>
 
 <?php
