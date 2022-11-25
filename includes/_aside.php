d7c9906691e96e41d2d6a45beaa31150e72657ff
MateusMachado01 <108525021+MateusMachado01@users.noreply.github.com>
2022-11-25 08:39:46 -0300

    recentes
    Co-Authored-By: Giovanna Pandolfo <108467195+GiovannaPandolfo@users.noreply.github.com>
    Co-Authored-By: Giovanna Rodrigues do Nascimento <108465696+gi-nascimento@users.noreply.github.com>

--- includes/_aside.php
+++ includes/_aside.php
@@ -1,9 +1,12 @@
 <aside class="col-lg-4 col-12">   
     <div clas="as">
     <h1 class="mais lidos"> Mais Lidos</h1>
+    <hr>
     <img class="imagem" src="img/lulinha.jpeg">
-    <p class= "aside-paragrafo">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</p>
+    <p class= "asideparagrafo">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</p>
+    <hr>
     <img class="imagem" src="img/barbie.jfif">
-    <p class="aside-paragrafo">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</p>
+    <p class="asideparagrafo">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</p>
+    <hr>
 </div>
 </aside>
