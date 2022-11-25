c695f58669a952f95638d50ac74e8673d8cc4c4c
Gustavo-Rossii <109541879+Gustavo-Rossii@users.noreply.github.com>
2022-11-25 09:57:35 -0300

    rossi

---
+++ artigo-viva-o-sus.php
@@ -0,0 +1,28 @@
+<?php
+include_once './includes/_header.php';
+?>
+<link rel="stylesheet" href="style.css">
+<main id="pgNoticia">
+    <h1 class="title">Viva o SUS!</h1>
+    <H3 class="subtitle">Sistema Único de Saúde (SUS), o direito de todos a saúde, o direito à vida.</H3>
+    <h6 class="h6">Por Gustavo Rossi</h6>
+    <h6 class="h62">25/11/2022 09:35</h6>
+    <hr>
+    <p class="conteudo">Pergunto-me o porquê de tamanha onda de pessoas contra algo que garante a vida e o bem-estar alheio. Talvez a resposta não exista, talvez a resposta seja o simples fato desse grupo de pessoas se abster da preocupação alheia, levando em consideração que esses têm condições financeiras para pagar um plano de saúde, diferente de grande parte da população brasileira. Mas será que essa elite está totalmente garantida com seus planos caso o SUS acabe? Será que eles estão totalmente protegidos? E se o dinheiro acabar? Com os outros desprotegidos, você não se desprotege?</p>
+    <img src="includes/img/sus1.png" alt="" class="img1">
+    <p class="conteudo">Muitos argumentam que não utilizam o SUS, que o sistema é falho, que têm dinheiro para bancar um plano de saúde, portanto a existência dele torna-se de nula importância. Porém, pode-se constatar vasta ignorância em tais argumentos, visto que o sistema evita que a maior parte da população seja esquecida e largada à mercê de doenças e demais complicações médicas.
+No ano de 2020, surgia a pandemia do vírus COVID-19. Por meio do SUS o brasileiro foi vacinado. Sem um programa de investimento em saúde, como temos hoje, não só o pobre, mas também o rico estariam desprotegidos, visto que para maior eficácia da vacina, o ideal é uma imunização em massa, não somente a quem tem condições para pagar a vacinação.</p>
+    <img src="includes/img/sus2.jpg" alt="" class="img2">
+    <p class="conteudo">O SUS está aí, não só prevenindo e curando doenças, mas também disponibilizando remédios a quem não tem condições de comprar.
+Assim concluo que é de suma importância a preservação do SUS, como único meio de dar a garantia de vida e de saúde a todos brasileiros, principalmente aos mais humildes e necessitados.
+</p>
+    
+</main>
+
+<?php
+
+include_once './includes/_aside.php';
+include_once './includes/_header.php';
+include_once './includes/_footer.php';
+?>
+
---
+++ noticia-lula-novo-presidente-da-republica.php
@@ -0,0 +1,25 @@
+<?php
+include_once './includes/_header.php';
+?>
+<link rel="stylesheet" href="style.css">
+<main id="pgNoticia">
+    <h1 class="title"></h1>
+    <H3 class="subtitle"></H3>
+    <h6 class="h6">Por </h6>
+    <h6 class="h62">25/11/2022 09:35</h6>
+    <hr>
+    <p class="conteudo"> aa </p>
+    <img src="includes/img/sus1.png" alt="" class="img1">
+    <p class="conteudo"> aa </p>
+    <img src="includes/img/sus2.jpg" alt="" class="img2">
+    <p class="conteudo"> aa </p>
+    
+</main>
+
+<?php
+
+include_once './includes/_aside.php';
+include_once './includes/_header.php';
+include_once './includes/_footer.php';
+?>
+
