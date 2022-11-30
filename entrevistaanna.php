007cabd522a8b87e3d359d5d49443c5e24e8d4df
Giovanna Nasimento <gigicasha@gmail.com>
2022-11-30 10:52:11 -0300

    Create entrevistaanna.php
    Co-Authored-By: Giovanna Pandolfo <108467195+GiovannaPandolfo@users.noreply.github.com>
    Co-Authored-By: Nathalia Alencastro <108465597+nathaliaalencastro@users.noreply.github.com>

---
+++ entrevistaanna.php
@@ -0,0 +1,48 @@
+<?php
+include_once './includes/_header.php';
+?>
+<link rel="stylesheet" href="style.css">
+<main id="pgNoticia">
+    <h1 class="title">Entrevista - Michele Loreto Alves</h1>
+    <H3 class="subtitle">Desafios do empreendedorismo feminino</H3>
+    <h6 class="h6">Por Anna Lia</h6>
+    <h6 class="h62">30/11/2022 09:35</h6>
+    <hr>
+    <p class="conteudo">A empresária Michele é a Loreto por trás da Don Loreto: pizzaria delivery localizada no coração da Cidade Baixa, em Porto Alegre (RS). A Don nasceu em 2016 com o objetivo de servir pizzas elaboradas de forma singular e artesanal com produtos de primeira qualidade, homenageando a tradição da família Loreto!  
+
+A equipe de produção, composta 100% por mulheres, oferece diariamente opções de pizzas e massas artesanais. 
+
+A dona e fundadora concedeu uma entrevista onde falou sobre sua história e os desafios do empreendedorismo feminino, incluindo dicas para quem quiser seguir o ramo. 
+    <p class="conteudo">De onde surgiu a Don Loreto?  
+
+Eu sempre trabalhei com vendas, e sempre fui apaixonada por trabalhar com metas e desafios. Trabalhei com todo tipo de produto, já fui vendedora de cadeira de rodas, fralda geriátrica, remédios, carros e patrocínios com leis de incentivo, o que acabou sendo o meu maior desafio. 
+
+Abri meu próprio negócio porque o mercado me levou a isso. Na época que trabalhava com captação de recursos o país entrou numa crise onde as empresas iriam ter menos recursos para investir em incentivo fiscal. Para eu não ficar parada ou perder muito a minha renda eu resolvi abrir meu próprio negócio.   
+
+Finalmente abri meu próprio negócio onde eu vendo pizzas e massas e crio minhas próprias receitas. É o produto mais difícil que eu já tive que vender, porque é um produto exclusivamente meu. .</p>
+    <img src="includes/img/sus2.jpg" alt="" class="img2">
+    <p class="conteudo"> Quais são os desafios sendo uma líder feminina? 
+
+É muito difícil ser uma mulher em qualquer circunstância. Na parte empresarial, tenho incontáveis experiencias negativas nesse sentido. Entre fornecedores, equipes parceiras e até clientes representando o machismo estrutural são acontecimentos diários. Existe uma diferença de tratamento entre empresários homens e mulheres. Fui desacreditada muitas vezes. Já tive uma equipe de funcionários de cozinha com homens, e era muito complicado o respeito pela liderança feminina. Hoje a Don Loreto é 100% composta de mulheres. Criamos mais oportunidades para a entrada no mundo do trabalho, e assim um ambiente seguro para todas nós trabalharmos. 
+
+Quero que outras mulheres tenham oportunidade de seguir o ramo, assim como eu.  
+
+ 
+</p>
+<p class="conteudo">Que dicas você daria para pessoas que querem seguir no ramo dos negócios, em específico, mulheres? 
+
+As pessoas abrem negócios com o pensamento de ter lucro e enriquecer em um ano, mas na verdade os negócios são a longo prazo. Eles precisam de um bom tempo para amadurecer.  
+
+Estamos agora há 6 anos com a pizzaria, e só agora entendo o suficiente o negócio para conseguir crescer. É natural e muito difícil alguém abrir um negócio e já no início entender profundamente. 
+
+A dica é: estudar o ramo em qual a pessoa vai entrar e conversar com pessoas que estejam abertas a contar os detalhes do ramo. Sempre pensei que se os outros estão tendo bons resultados nas empresas, é um sinal de que o mercado está bom e que eu posso ficar bem também. Quanto mais o meu vizinho for próspero, melhor para mim também. 
+
+ </p>
+</main>
+
+<?php
+
+include_once './includes/_header.php';
+include_once './includes/_footer.php';
+?>
+
