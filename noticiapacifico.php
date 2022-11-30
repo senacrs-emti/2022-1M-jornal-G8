c1cac5fed6279acef7bec6adcf65949af4eb6e42
Giovanna Nasimento <gigicasha@gmail.com>
2022-11-30 09:49:41 -0300

    Create noticiapacifico.php
    Co-Authored-By: Giovanna Pandolfo <108467195+GiovannaPandolfo@users.noreply.github.com>

---
+++ noticiapacifico.php
@@ -0,0 +1,24 @@
+<?php
+include_once './includes/_header.php';
+?>
+<link rel="stylesheet" href="style.css">
+<main id="pgNoticia">
+    <h1 class="title">A Ilha de Lixo no Pacífico</h1>
+    <H3 class="subtitle">Deveríamos limpar a ilha de lixo no pacífico?</H3>
+    <h6 class="h6">Por Giovanna Pandolfo</h6>
+    <h6 class="h62">09/11/2022 09:24</h6>
+    <hr>
+    <p class="conteudo">Alguns ecologistas e biólogos dizem e pensam que em meio a todo o lixo, também vivem seres vivos como dragões -marinhos azuis, veleiros, caravelas-portuguesas, porpitas, hidrozoários, caracóis roxos entre outros animais, os mesmos são conhecidos como nêuston.</p>
+    <img src="includes/img/plastico.jpg" alt="" class="img1">
+    <p class="conteudo">Nadadores observam, que todo o lixo e plástico, estava envolvido com vidas ao seu redor, como uma sopa de garrafas de plástico, redes de pesca, pneus e escovas de dentes, e na superfície flutuavam todos os animais citados acima no texto, principalmente dragões-marinhos azuis, e uma comunidade de nêustons dentro de todo esse lixo, chegando a haver mais destes animais do que plásticos.</p>
+    <img src="includes/img/ilhalixo.webp" alt="" class="img2">
+    <p class="conteudo">A grande mancha de lixo do Pacífico, que se encontra entre o Havaí e a Califórnia, contém pelo menos 79 toneladas de plástico, segundo a Ocean Cleanup Foundation. Mas essa mancha também se tornou um ponto de encontro para diversos organismos marinhos, colocando na balança limpar o oceano pacífico pode proteger e salvar vidas de mamíferos e aves marinhas, porém terá um grande número de mortes destes outros animais que acabaram se apropriando e morando nesse lixo que precisa ser retirado.A grande mancha de lixo do Pacífico, que se encontra entre o Havaí e a Califórnia, contém pelo menos 79 toneladas de plástico, segundo a Ocean Cleanup Foundation. Mas essa mancha também se tornou um ponto de encontro para diversos organismos marinhos, colocando na balança limpar o oceano pacífico pode proteger e salvar vidas de mamíferos e aves marinhas, porém terá um grande número de mortes destes outros animais que acabaram se apropriando e morando nesse lixo que precisa ser retirado.A grande mancha de lixo do Pacífico, que se encontra entre o Havaí e a Califórnia, contém pelo menos 79 toneladas de plástico, segundo a Ocean Cleanup Foundation. Mas essa mancha também se tornou um ponto de encontro para diversos organismos marinhos, colocando na balança limpar o oceano pacífico pode proteger e salvar vidas de mamíferos e aves marinhas, porém terá um grande número de mortes destes outros animais que acabaram se apropriando e morando nesse lixo que precisa ser retirado.</p>
+    
+</main>
+
+<?php
+
+include_once './includes/_aside.php';
+include_once './includes/_header.php';
+include_once './includes/_footer.php';
+?>
